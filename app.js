48fc2395bdc5e98c7b500a52f8b5d7e3cd47dc63
Nafis Fuad <fuadbd14@gmail.com>
2019-09-07 20:15:43 +0600

    Update Error handling

--- app.js
+++ app.js
@@ -14,15 +14,25 @@ app.use('/', home)
 app.use('/closest', closest)
 
 // Handle Route errors
+
+// not found error
+app.use((req, res, next) => {
+  const error = new Error()
+  error.status = 404
+  next(error)
+})
+
 app.use((err, req, res, next) => {
   if (err.status === 404) {
-    return res.status(400).json({
+    return res.status(404).json({
       message: 'Route Not Found',
+      error: err,
     })
   }
   if (err.status === 500) {
     return res.status(500).json({
       message: 'Internal Server Error',
+      error: err,
     })
   }
   next()
